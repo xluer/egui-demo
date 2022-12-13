72ea493aca32d7b45e170204b0643a6a3e9dc00f
xluer <xluer@163.com>
2022-12-13 07:23:56 +0000

    中文支持

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,5 @@
+use egui::{FontDefinitions, FontData, FontFamily};
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
@@ -14,7 +16,7 @@ impl Default for TemplateApp {
     fn default() -> Self {
         Self {
             // Example stuff:
-            label: "Hello World!".to_owned(),
+            label: "中文123".to_owned(),
             value: 2.7,
         }
     }
@@ -25,7 +27,23 @@ impl TemplateApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customized the look at feel of egui using
         // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
-
+        let mut fonts = FontDefinitions::default();
+        
+        fonts.font_data.insert(
+            String::from("s_chinese_fallback"),
+            FontData::from_static(include_bytes!("resources/NotoSansSC-Regular.otf")),
+        );
+        fonts
+            .families
+            .get_mut(&FontFamily::Proportional)
+            .unwrap()
+            .push("s_chinese_fallback".to_owned());
+        fonts
+            .families
+            .entry(egui::FontFamily::Proportional)
+            .or_default()
+            .insert(0, "s_chinese_fallback".to_owned());
+        cc.egui_ctx.set_fonts(fonts);
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
         if let Some(storage) = cc.storage {
@@ -56,8 +74,8 @@ impl eframe::App for TemplateApp {
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
             egui::menu::bar(ui, |ui| {
-                ui.menu_button("File", |ui| {
-                    if ui.button("Quit").clicked() {
+                ui.menu_button("文件", |ui| {
+                    if ui.button("退出").clicked() {
                         _frame.close();
                     }
                 });
@@ -65,24 +83,24 @@ impl eframe::App for TemplateApp {
         });
 
         egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
+            ui.heading("边栏");
 
             ui.horizontal(|ui| {
-                ui.label("Write something: ");
+                ui.label("输入内容: ");
                 ui.text_edit_singleline(label);
             });
 
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
+            ui.add(egui::Slider::new(value, 0.0..=10.0).text("值"));
+            if ui.button("增加").clicked() {
                 *value += 1.0;
             }
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
                 ui.horizontal(|ui| {
                     ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
+                    ui.label("基于 ");
                     ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
+                    ui.label(" ");
                     ui.hyperlink_to(
                         "eframe",
                         "https://github.com/emilk/egui/tree/master/crates/eframe",
@@ -95,17 +113,17 @@ impl eframe::App for TemplateApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
 
-            ui.heading("eframe template");
+            ui.heading("eframe 模板");
             ui.hyperlink("https://github.com/emilk/eframe_template");
             ui.add(egui::github_link_file!(
                 "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
+                "源代码"
             ));
             egui::warn_if_debug_build(ui);
         });
 
         if false {
-            egui::Window::new("Window").show(ctx, |ui| {
+            egui::Window::new("窗口").show(ctx, |ui| {
                 ui.label("Windows can be moved by dragging them.");
                 ui.label("They are automatically sized based on contents.");
                 ui.label("You can turn on resizing and scrolling if you like.");
